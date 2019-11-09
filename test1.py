0bbb50a99f9bfffafd05bcd4e5c0c7552d35148e
yukun <caoyukun0430@yeah.net>
2019-11-09 12:02:14 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -20,8 +20,8 @@ def get_fit_prec_hpc(log):
     return fitness,precision
 
 
-log = xes_importer.apply("/home/yukun/dataset/BPIC2017.xes")
-sublog2 = xes_importer.apply("/home/yukun/resultlog/log_6_2_dfgRequestedAmount.xes")
+log = xes_importer.apply("/home/yukun/dataset/filteredbpic2017.xes")
+sublog2 = xes_importer.apply("/home/yukun/resultlog/log_3_1_DMMCreditScore.xes")
 #log = parquet_importer.import_minimal_log("bpic2017_application.parquet")
 print("imported")
 net, im, fm = inductive_miner.apply(sublog2)
@@ -30,32 +30,8 @@ net, im, fm = inductive_miner.apply(sublog2)
 #pn_vis_factory.view(gviz)
 print("calculated model")
 wrapper = remote_wrapper_factory.apply("137.226.117.71", "5001", "hello", "DUMMYDUMMY")
-aa = time.time()
-# fitness = wrapper.calculate_fitness_with_tbr(net, im, fm, log)
-# fitness = wrapper.calculate_fitness_with_alignments(net, im, fm, log, parameters={"align_variant": "state_equation_a_star"})['averageFitness']
-# bb = time.time()
-# print(fitness)
-# fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
-# print("local",fitness)
-
-precision = wrapper.calculate_precision_with_tbr(net, im, fm, log)
-print("cluster",precision)
-
-precision = precision_factory.apply(log, net, im, fm)
-print("local",precision)
-
-log = xes_importer.apply("/home/yukun/dataset/BPIC2017.xes")
-sublog2 = xes_importer.apply("/home/yukun/resultlog/log_6_3_dfgRequestedAmount.xes")
-#log = parquet_importer.import_minimal_log("bpic2017_application.parquet")
-print("imported")
-net, im, fm = inductive_miner.apply(sublog2)
-# net, im, fm = petri_importer.apply("/home/yukun/dataset/sublog2.pnml")
-#gviz = pn_vis_factory.apply(net, im, fm)
-#pn_vis_factory.view(gviz)
-print("calculated model")
-wrapper = remote_wrapper_factory.apply("137.226.117.71", "5001", "hello", "DUMMYDUMMY")
-aa = time.time()
-# fitness = wrapper.calculate_fitness_with_tbr(net, im, fm, log)
+# aa = time.time()
+# # fitness = wrapper.calculate_fitness_with_tbr(net, im, fm, log)
 # fitness = wrapper.calculate_fitness_with_alignments(net, im, fm, log, parameters={"align_variant": "state_equation_a_star"})['averageFitness']
 # bb = time.time()
 # print(fitness)
