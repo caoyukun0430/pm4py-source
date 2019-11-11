e1eb69a005d213bd6a55c6a0da240e80e1609db3
yukun <caoyukun0430@yeah.net>
2019-11-11 23:33:09 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -49,11 +49,88 @@ def get_fit_prec_hpc(log):
 
 # precision = precision_factory.apply(log, net, im, fm)
 # print("local",precision)
-
-log = xes_importer.apply("/home/yukun/dataset/document_logs/Geo_parcel_document.xes")
+log = xes_importer.apply("/home/yukun/dataset/Receipt.xes")
+sublog = xes_importer.apply("/home/yukun/resultlog/Receipt/responsible/log_4_0_dfgresponsible.xes")
 # sublog = xes_importer.apply("/home/yukun/dataset/sublog_receipt.xes")
 print("imported")
-net, im, fm = inductive_miner.apply(log)
+net, im, fm = inductive_miner.apply(sublog)
+# net, im, fm = petri_importer.apply("/home/yukun/dataset/sublog2.pnml")
+#gviz = pn_vis_factory.apply(net, im, fm)
+#pn_vis_factory.view(gviz)
+print("calculated model")
+wrapper = remote_wrapper_factory.apply("137.226.117.71", "5001", "hello", "DUMMYDUMMY")
+aa = time.time()
+# fitness = wrapper.calculate_fitness_with_tbr(net, im, fm, log)
+fitness = wrapper.calculate_fitness_with_alignments(net, im, fm, log, parameters={"align_variant": "state_equation_a_star"})['averageFitness']
+bb = time.time()
+print(fitness)
+# fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
+# print("local",fitness)
+precision = wrapper.calculate_precision_with_tbr(net, im, fm, log)
+print("prec",precision)
+
+fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
+print("local",fitness)
+
+precision = precision_factory.apply(log, net, im, fm)
+print("prec",precision)
+
+
+sublog = xes_importer.apply("/home/yukun/resultlog/Receipt/responsible/log_4_1_dfgresponsible.xes")
+# sublog = xes_importer.apply("/home/yukun/dataset/sublog_receipt.xes")
+print("imported")
+net, im, fm = inductive_miner.apply(sublog)
+# net, im, fm = petri_importer.apply("/home/yukun/dataset/sublog2.pnml")
+#gviz = pn_vis_factory.apply(net, im, fm)
+#pn_vis_factory.view(gviz)
+print("calculated model")
+wrapper = remote_wrapper_factory.apply("137.226.117.71", "5001", "hello", "DUMMYDUMMY")
+aa = time.time()
+# fitness = wrapper.calculate_fitness_with_tbr(net, im, fm, log)
+fitness = wrapper.calculate_fitness_with_alignments(net, im, fm, log, parameters={"align_variant": "state_equation_a_star"})['averageFitness']
+bb = time.time()
+print(fitness)
+# fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
+# print("local",fitness)
+precision = wrapper.calculate_precision_with_tbr(net, im, fm, log)
+print("prec",precision)
+
+fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
+print("local",fitness)
+
+precision = precision_factory.apply(log, net, im, fm)
+print("prec",precision)
+
+
+sublog = xes_importer.apply("/home/yukun/resultlog/Receipt/responsible/log_4_2_dfgresponsible.xes")
+# sublog = xes_importer.apply("/home/yukun/dataset/sublog_receipt.xes")
+print("imported")
+net, im, fm = inductive_miner.apply(sublog)
+# net, im, fm = petri_importer.apply("/home/yukun/dataset/sublog2.pnml")
+#gviz = pn_vis_factory.apply(net, im, fm)
+#pn_vis_factory.view(gviz)
+print("calculated model")
+wrapper = remote_wrapper_factory.apply("137.226.117.71", "5001", "hello", "DUMMYDUMMY")
+aa = time.time()
+# fitness = wrapper.calculate_fitness_with_tbr(net, im, fm, log)
+fitness = wrapper.calculate_fitness_with_alignments(net, im, fm, log, parameters={"align_variant": "state_equation_a_star"})['averageFitness']
+bb = time.time()
+print(fitness)
+# fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
+# print("local",fitness)
+precision = wrapper.calculate_precision_with_tbr(net, im, fm, log)
+print("prec",precision)
+
+fitness = replay_factory.apply(log, net, im, fm, variant="alignments")['averageFitness']
+print("local",fitness)
+
+precision = precision_factory.apply(log, net, im, fm)
+print("prec",precision)
+
+sublog = xes_importer.apply("/home/yukun/resultlog/Receipt/responsible/log_4_3_dfgresponsible.xes")
+# sublog = xes_importer.apply("/home/yukun/dataset/sublog_receipt.xes")
+print("imported")
+net, im, fm = inductive_miner.apply(sublog)
 # net, im, fm = petri_importer.apply("/home/yukun/dataset/sublog2.pnml")
 #gviz = pn_vis_factory.apply(net, im, fm)
 #pn_vis_factory.view(gviz)
