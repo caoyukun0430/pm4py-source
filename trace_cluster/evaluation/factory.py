60f3ca06e75a96c661c372c35a15d9d37f0b1fba
yukun <caoyukun0430@yeah.net>
2019-11-20 16:40:29 +0100

    modify factory for front end

--- trace_cluster/evaluation/factory.py
+++ trace_cluster/evaluation/factory.py
@@ -12,6 +12,7 @@ from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.objects.log.log import EventLog
 from pm4py.util import constants
 from pm4py.algo.filtering.log.attributes import attributes_filter
+from pm4py.algo.discovery.inductive import factory as inductive_miner
 from trace_cluster.evaluation import fake_log_eval
 from pm4py.algo.discovery.alpha import factory as alpha_miner
 from pm4py.visualization.petrinet import factory as pn_vis_factory
@@ -22,10 +23,29 @@ VARIANT_DMM_LEVEN = "variant_DMM_leven"
 VARIANT_AVG_LEVEN = "variant_avg_leven"
 VARIANT_DMM_VEC = "variant_DMM_vec"
 VARIANT_AVG_VEC = "variant_avg_vec"
+DFG = 'dfg'
 
 VERSION_METHODS = {VARIANT_DMM_LEVEN: fake_log_eval.eval_DMM_leven, VARIANT_AVG_LEVEN: fake_log_eval.eval_avg_leven,
-                   VARIANT_DMM_VEC: fake_log_eval.eval_DMM_variant, VARIANT_AVG_VEC: fake_log_eval.eval_avg_variant}
+                   VARIANT_DMM_VEC: fake_log_eval.eval_DMM_variant, VARIANT_AVG_VEC: fake_log_eval.eval_avg_variant,
+                   DFG:fake_log_eval.dfg_dis}
 
+
+def bfs(tree):
+    queue = []
+    output = []
+    queue.append(tree)
+    while queue:
+        # element in queue is waiting to become root and splited into child
+        # root is the first ele of queue
+        root = queue.pop(0)
+        if len(root['children']) > 0:
+            name = [root['name']]
+            for child in root['children']:
+                queue.append(child)
+                name.append(child['name'])
+            output.append(name)
+
+    return output
 
 def apply(log, variant=VARIANT_DMM_LEVEN, parameters=None):
     if parameters is None:
@@ -36,14 +56,14 @@ def apply(log, variant=VARIANT_DMM_LEVEN, parameters=None):
 
     list_of_vals = []
     list_log = []
-    list_of_vals_dict = attributes_filter.get_trace_attribute_values(log, 'index')
+    list_of_vals_dict = attributes_filter.get_trace_attribute_values(log, 'responsible')
 
     list_of_vals_keys = list(list_of_vals_dict.keys())
     for i in range(len(list_of_vals_keys)):
         list_of_vals.append(list_of_vals_keys[i])
 
     for i in range(len(list_of_vals)):
-        logsample = merge_log.log2sublog(log, list_of_vals[i])
+        logsample = merge_log.log2sublog(log, list_of_vals[i],'responsible')
         list_log.append(logsample)
 
     if variant in VERSION_METHODS:
@@ -54,12 +74,12 @@ def apply(log, variant=VARIANT_DMM_LEVEN, parameters=None):
     # Create dictionary for labeling nodes by their IDs
 
     id2name = dict(zip(range(len(list_of_vals)), list_of_vals))
-    print("id",id2name)
+    # print("id",id2name)
 
     T = to_tree(Z, rd=False)
     d3Dendro = dict(children=[], name="Root1")
     merge_log.add_node(T, d3Dendro)
-    print("d3", d3Dendro)
+    # print("d3", d3Dendro)
 
     leafname = merge_log.label_tree(d3Dendro["children"][0], id2name)
     print("leafname",leafname)
@@ -85,22 +105,6 @@ def apply(log, variant=VARIANT_DMM_LEVEN, parameters=None):
 
 
     # get the triple group of nodes
-    def bfs(tree):
-        queue =[]
-        output = []
-        queue.append(tree)
-        while queue:
-            #element in queue is waiting to become root and splited into child
-            # root is the first ele of queue
-            root = queue.pop(0)
-            if len(root['children'])>0:
-                name = [root['name']]
-                for child in root['children']:
-                    queue.append(child)
-                    name.append(child['name'])
-                output.append(name)
-
-        return output
 
 
 
@@ -119,14 +123,15 @@ def apply(log, variant=VARIANT_DMM_LEVEN, parameters=None):
 
 
     # selectedNode='1-2-3-4'
+    # selectedNode='Resource01-Resource02-Resource03-Resource04-Resource05-Resource06-Resource08-Resource09-Resource11-Resource12-Resource13-Resource14-Resource20-Resource21-Resource22-Resource23-Resource24-Resource25-Resource28-Resource29-Resource32-Resource37-Resource40-Resource07-Resource35'
     #
     # select_index = rootlist.index(selectedNode)
     # show_triple = trilist[select_index]
-    # print(show_triple)
+    # print('show_triple',show_triple)
     # slice_val = []
     # for ele in show_triple:
     #     slice_val.append(ele.split('-'))
-    # print(slice_val)
+    # print('slice_val',slice_val)
 
 
     # plt.figure(figsize=(10, 8))
@@ -143,16 +148,25 @@ def apply(log, variant=VARIANT_DMM_LEVEN, parameters=None):
 
 
 if __name__ == "__main__":
-    percent = 1
-    alpha = 0.5
-    (loglist, mergedlog) = merge_log.merge_log("C:\\Users\\yukun\\PycharmProjects\\PTandLogGenerator\\data\\logs", 4, 2)
+    # percent = 1
+    # alpha = 0.5
+    # LOG_PATH = "D:/Sisc/19SS/thesis/Dataset/Receipt4.xes"
+    # ATTR_NAME = 'responsible'
+    # METHOD = 'DMM'
+    #
+    # # PIC_PATH = 'D:/Sisc/19SS/thesis/Dataset/'
+    # mergedlog = xes_importer.apply(LOG_PATH)
 
-    # mergedlog = xes_importer.apply("C:\\Users\\yukun\\PycharmProjects\\pm4py-ws\\logs\\mergedlog_2_J235.xes")
-    (ret,leafname)=apply(mergedlog, variant=VARIANT_DMM_LEVEN)
+    # (loglist, mergedlog) = merge_log.merge_log("C:\\Users\\yukun\\PycharmProjects\\PTandLogGenerator\\data\\logs", 4, 2)
 
-    slice_num = 4
-    index_list=[]
-    length = int(len(leafname) / slice_num)
+    # mergedlog = xes_importer.apply("C:\\Users\\yukun\\PycharmProjects\\pm4py-ws\\logs\\mergedlog_2_2EZM.xes")
+    mergedlog = xes_importer.apply("D:/Sisc/19SS/thesis/Dataset/Receipt4.xes")
+    (ret,leafname)=apply(mergedlog, variant=DFG)
+
+
+    # slice_num = 4
+    # index_list=[]
+    # length = int(len(leafname) / slice_num)
     # for i in range(0,slice_num):
     #     index_list.append(int(i*length))
     # print(index_list)
@@ -162,20 +176,39 @@ if __name__ == "__main__":
     # for i in range(slice_num):
     #     slice_val.append(leafname[i*length:(i+1)*length])
     # print(slice_val)
-    slice_val = [['1', '2', '3', '4'], ['1', '2'], ['3', '4']]
 
-    gviz_list=[]
+
+    # slice_val = [['1', '2', '3', '4'], ['1', '2'], ['3', '4']]
+    #
+    trilist = bfs(ret)
+    trilist[0][0] = trilist[0][1] + '-' + trilist[0][2]
+
+    rootlist = []
+    for ele in trilist:
+        rootlist.append(ele[0])
+    selectedNode = 'Resource01-Resource02-Resource03-Resource04-Resource05-Resource06-Resource08-Resource09-Resource11-Resource12-Resource13-Resource14-Resource20-Resource21-Resource22-Resource23-Resource24-Resource25-Resource28-Resource29-Resource32-Resource37-Resource40-Resource07-Resource35'
+    #
+    select_index = rootlist.index(selectedNode)
+    show_triple = trilist[select_index]
+    print('show_triple',show_triple)
+    slice_val = []
+    for ele in show_triple:
+        slice_val.append(ele.split('-'))
+    print('slice_val',slice_val)
+    # gviz_list=[]
     for i in range(len(slice_val)):
-        logsample = merge_log.logslice(mergedlog, slice_val[i])
-        net, initial_marking, final_marking = alpha_miner.apply(logsample)
+        logsample = merge_log.logslice(mergedlog, slice_val[i],'responsible')
+        net, initial_marking, final_marking = inductive_miner.apply(logsample)
         parameters = {"format": "svg"}
         gviz = pn_vis_factory.apply(net, initial_marking, final_marking,parameters={"format":"svg"})
         filenname = "C:\\Users\yukun\\PycharmProjects\\pm4py-source\\trace_cluster\\evaluation\\"+"pn"+str(i+1)+".svg"
         print(filenname)
         pn_vis_factory.save(gviz, filenname)
-        gviz_list.append({"name":filenname})
+    #     gviz_list.append({"name":filenname})
+    #
+    # print("list",gviz_list)
+    # for gviz in gviz_list:
+    #     print(gviz)
 
-    print("list",gviz_list)
-    for gviz in gviz_list:
-        print(gviz)
+
 
