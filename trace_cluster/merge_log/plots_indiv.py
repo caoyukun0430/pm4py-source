979be71ec24401ad50f228d5a7790d0b3e7cb1b5
yukun <caoyukun0430@yeah.net>
2020-01-27 17:48:50 +0100

    do not change yukun-paper
    run cluster only on branch dev-yukun

--- trace_cluster/merge_log/plots_indiv.py
+++ trace_cluster/merge_log/plots_indiv.py
@@ -287,53 +287,12 @@ def example_run(LOG_PATH, ATTR_NAME, METHOD, PIC_PATH, plot_clu):
 
 if __name__ == "__main__":
     # LOG_PATH = "../example/real_log/Receipt.xes"
-    # LOG_PATH = '../../tests/input_data/receipt.xes'
-    LOG_PATH='/home/yukun/dataset/Receipt.xes'
-    ATTR_NAME = 'responsible'
-    PIC_PATH = '/home/yukun/resultlog/'
+    LOG_PATH = '../../tests/input_data/receipt.xes'
+    ATTR_NAME = 'group'
+    PIC_PATH = '../example/real_log/'
     METHOD = 'dfg'
-    plot_clu = 37
-    # example_run(LOG_PATH, ATTR_NAME, METHOD, PIC_PATH, plot_clu)
-    percent = 1
-    alpha = 0.5
-    runtime = dict()
-
-    print(LOG_PATH)
-    print(ATTR_NAME)
-    print(METHOD)
-    log = xes_importer.apply(LOG_PATH)
-    TYPE = METHOD + ATTR_NAME + 'update'
-
-    (plot_fit, plot_prec, plot_F1, plot_boxfit, plot_boxprec, plot_box, plot_length,
-     runtime) = merge_log.main_calc_recompute(log, ATTR_NAME, METHOD, TYPE, PIC_PATH, percent, alpha, runtime, plot_clu)
-    F1valup = list(plot_F1.values())
-
-    TYPE = METHOD + ATTR_NAME
-
-    (plot_fit, plot_prec, plot_F1, plot_boxfit, plot_boxprec, plot_box, plot_length,
-     runtime) = merge_log.main_calc_recompute(log, ATTR_NAME, METHOD, TYPE, PIC_PATH, percent, alpha, runtime, plot_clu)
-
-    F1val = list(plot_F1.values())
-    F1dfg = [F1val, F1valup]
-    print('F1compare', F1dfg)
-    x_axis = range(1, plot_clu + 1)
-
-    fig9 = plt.figure()
-    rc('text', usetex=True)
-    rc('font', family='serif')
-    l1 = plt.plot(x_axis, F1dfg[0], color="b", linestyle="-", marker="s", linewidth=1)
-    l2 = plt.plot(x_axis, F1dfg[1], color="r", linestyle="-", marker="o", linewidth=1)
-    plt.xticks(x_axis,fontsize=7)
-    # plt.gca().invert_xaxis()
-    plt.ylim(0, 1.04)
-    plt.legend([l1, l2], labels=['Behav. CL.', 'Behav. CL-recomputation'], loc='best')
-    # plt.title('Leven-Recomputing')
-    plt.xlabel("Num. of Cluster")
-    plt.ylabel("F1-Score")
-    plt.grid(axis='y')
-    plt.savefig(PIC_PATH + 'Behav. CL-recomputationornot' + '.svg')
-
-
+    plot_clu = 5
+    example_run(LOG_PATH, ATTR_NAME, METHOD, PIC_PATH, plot_clu)
 
 
     # standard_plt(LOG_PATH, ATTR_NAME, PIC_PATH,plot_clu)
